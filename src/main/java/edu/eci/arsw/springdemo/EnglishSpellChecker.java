037ce4e48ffc4c4ab144c7fce46da8c23a96dc66
Santiago Gualdron <santiago.gualdron-r@mail.escuelaing.edu.co>
2025-09-06 08:47:39 -0500

    generacion del ultimo punto

--- src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -2,7 +2,7 @@ package edu.eci.arsw.springdemo;
 
 import org.springframework.stereotype.Service;
 
-@Service // class that'll be inyected
+//@Service // class that'll be inyected
 public class EnglishSpellChecker implements SpellChecker {
 
 	@Override
